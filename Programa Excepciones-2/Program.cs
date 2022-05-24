dc8cd029ee98d064d4700312d38e8c80a6898db1
Yahirs19 <L21212055@tectijuana.edu.mx>
2022-05-24 11:15:28 -0700

    Update

---
+++ Programa Excepciones-2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programa_Excepciones_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Indique un numero: ");
+                int num1 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine(Math.Pow(num1, 2));
+                Console.ReadKey();
+            }
+            catch(FormatException)
+            {
+                Console.WriteLine("El tipo de dato debe ser un numero entero");
+            }
+            catch(OverflowException)
+            {
+                Console.WriteLine("El valor asignado es demasiado grande para el tipo de dato");
+            }
+        }
+    }
+}
